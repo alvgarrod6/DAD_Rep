e20e3ee0ae09b216e34547d8c39cd14695d27310
alvgarrod6 <alvgarrod6@alum.us.es>
2020-04-03 12:21:25 +0200

    modificacion de la clase usuario

--- PruebaMaven1/src/main/java/types/Usuario.java
+++ PruebaMaven1/src/main/java/types/Usuario.java
@@ -1,79 +1,70 @@
 package types;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class Usuario {
-	private static final AtomicInteger COUNTER = 
-			new AtomicInteger();
 	
 	private int id;
+	private String user;
+	private String pass;
 	private String nombre;
 	private String apellidos;
 	private String dni;
-	private String telefono;
+	private long nacimiento;
 	
-	@JsonCreator	
-	public Usuario(
-			@JsonProperty("nombre") String nombre, 
-			@JsonProperty("apellidos") String apellidos,
-			@JsonProperty("dni") String dni,
-			@JsonProperty("telefono") String telefono){
+	public Usuario(int id, String user, String pass, String nombre, String apellidos, String dni, long nacimiento) {
 		super();
-		this.id = COUNTER.getAndIncrement();
+		this.id = id;
+		this.user = user;
+		this.pass = pass;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.dni = dni;
-		this.telefono = telefono;
+		this.nacimiento = nacimiento;
 	}
-
 	public Usuario() {
 		super();
-		this.id = COUNTER.getAndIncrement();
-		this.nombre = "";
-		this.apellidos = "";
-		this.dni = "";
-		this.telefono = "";
 	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellidos() {
-		return apellidos;
-	}
-
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
 	public int getId() {
 		return id;
 	}
-
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public long getNacimiento() {
+		return nacimiento;
+	}
+	public void setNacimiento(long nacimiento) {
+		this.nacimiento = nacimiento;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -81,11 +72,12 @@ public class Usuario {
 		result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
 		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
 		result = prime * result + id;
+		result = prime * result + (int) (nacimiento ^ (nacimiento >>> 32));
 		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
-		result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
+		result = prime * result + ((pass == null) ? 0 : pass.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -107,18 +99,27 @@ public class Usuario {
 			return false;
 		if (id != other.id)
 			return false;
+		if (nacimiento != other.nacimiento)
+			return false;
 		if (nombre == null) {
 			if (other.nombre != null)
 				return false;
 		} else if (!nombre.equals(other.nombre))
 			return false;
-		if (telefono == null) {
-			if (other.telefono != null)
+		if (pass == null) {
+			if (other.pass != null)
 				return false;
-		} else if (!telefono.equals(other.telefono))
+		} else if (!pass.equals(other.pass))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
 			return false;
 		return true;
 	}
+	
+	
 	
 	
 	
